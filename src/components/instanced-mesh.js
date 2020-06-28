fb1ca88590c63019a95ffa781a8dfc6693b139a5
Kieran Farr <kieran.farr@gmail.com>
2020-06-27 23:32:44 -0700

    add verbose logging and attempts to make instancing work on gltf part from mixin

--- src/components/instanced-mesh.js
+++ src/components/instanced-mesh.js
@@ -13,10 +13,15 @@ AFRAME.registerComponent('instanced-mesh', {
   },
 
   update: function () {
+    console.log("lalalala starting instanced-mesh");
+    console.log(this.el.hasAttribute("create-from-json"));
+    
+
     var self = this;
     var el = this.el;
     var list = this.el.children;
     var quantity = 0;
+    var geometry, material;
 
     var applyMatrix = (function () {
       var position = new THREE.Vector3();
@@ -35,23 +40,58 @@ AFRAME.registerComponent('instanced-mesh', {
         scale.y = el.children[i].object3D.scale.y;
         scale.z = el.children[i].object3D.scale.z;
         matrix.compose(position, quaternion, scale);
+        console.log(i, el.children[i].object3D);        
       }; // High verbosity because imma N00b don´t know how to access matrix on an uninitialized object
     }());
+
     for (var item of list) {
       quantity = quantity + 1;
     }
+    console.log(quantity);
+
+    // if this element has create-from-json component, wait until 'create-from-json-complete' is emitted 
+    if (this.el.hasAttribute("create-from-json") && (quantity < 1)) {
+      console.log("has attribute create-from-json and quantity < 1!");
+      this.el.addEventListener('create-from-json-complete', e => {
+        this.update.call(this, this.data);
+      });
+      return;
+    }
+    
     var mesh = this.el.getObject3D('mesh');
     if (!mesh) {
+      console.log("no mesh");
+
       this.el.addEventListener('model-loaded', e => {
         this.update.call(this, this.data);
       });
       return;
     }
-    var material = mesh.material.clone();
+    console.log(mesh);
+    
+    // var material = mesh.material.clone();
 
     mesh.traverse(function (node) {
-      if (node.type != 'Mesh') return;
-      geometry = node.geometry;
+      console.log(node.name);
+      
+      if (node.type != 'Mesh') {
+        console.log("no mesh");  
+        return;
+      }
+
+      console.log(node.geometry);
+      console.log(node.material);
+
+
+      if (node.geometry && node.material) {
+        console.log("runme");
+        
+        geometry = node.geometry;
+        material = node.material.clone();
+      } else {
+        console.log("no material or geometry");
+        return;        
+      }
     });
 
     var amesh = new THREE.InstancedMesh(geometry, material, quantity);
@@ -61,7 +101,7 @@ AFRAME.registerComponent('instanced-mesh', {
       child = this.el.children[i];
       applyMatrix(i, matrix);
       amesh.setMatrixAt(i, matrix);
-
+      console.log("adding stuff");
       if (this.data.center) {
         amesh.geometry.center();
         if (this.data.bottomAlign) {
