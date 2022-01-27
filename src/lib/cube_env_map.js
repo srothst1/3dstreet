e8258be17fbd3344d06b194712fbc42f36a10fec
Samuel Rothstein <samrothstein98@gmail.com>
2022-01-27 12:27:23 -0800

    added cube env map algorithm

---
+++ src/lib/cube_env_map.js
@@ -0,0 +1,41 @@
+AFRAME.registerComponent("cube-env-map", {
+    schema: {
+        path: {default: ''},
+        extension: {default: 'jpg'},
+        format: {default: 'RGBFormat'},
+        enableBackground: {default: false}
+    },
+
+    init: function () {
+        const data = this.data;
+        var posx = data.path.split(" ")[0].replace(/\s+/g, '') + 'posx.' + data.extension.split(" ")[0].replace(/\s+/g, '');
+        var negx = data.path.split(" ")[0].replace(/\s+/g, '') + 'negx.' + data.extension.split(" ")[0].replace(/\s+/g, '');
+        var posy = data.path.split(" ")[0].replace(/\s+/g, '') + 'posy.' + data.extension.split(" ")[0].replace(/\s+/g, '');
+        var negy = data.path.split(" ")[0].replace(/\s+/g, '') + 'negy.' + data.extension.split(" ")[0].replace(/\s+/g, '');
+        var posz = data.path.split(" ")[0].replace(/\s+/g, '') + 'posz.' + data.extension.split(" ")[0].replace(/\s+/g, '');
+        var negz = data.path.split(" ")[0].replace(/\s+/g, '') + 'negz.' + data.extension.split(" ")[0].replace(/\s+/g, '');
+        this.texture = new THREE.CubeTextureLoader().load([posx,negx,posy,negy,posz,negz]);
+        this.texture.format = THREE[data.format];
+
+        if (data.enableBackground) {
+        this.el.sceneEl.object3D.background = this.texture;
+        }
+
+        this.applyEnvMap();
+        this.el.addEventListener('object3dset', this.applyEnvMap.bind(this));
+    },
+
+    applyEnvMap: function () {
+        const mesh = this.el.getObject3D('mesh');
+        const envMap = this.texture;
+
+        if (!mesh) return;
+
+        mesh.traverse(function (node) {
+        if (node.material && 'envMap' in node.material) {
+            node.material.envMap = envMap;
+            node.material.needsUpdate = true;
+        }
+        });
+    }
+    });
